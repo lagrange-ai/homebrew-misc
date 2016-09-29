063dbfd83ba96ee686a6400d6a210a49154ee253
Vlad Ki <proger@wilab.org.ua>
2016-09-29 12:39:10 +0300

    wget-lua patches

--- wget-lua.rb
+++ wget-lua.rb
@@ -1,7 +1,7 @@
 class WgetLua < Formula
   desc "wget-lua"
-  url "http://warriorhq.archiveteam.org/downloads/wget-lua/wget-1.14.lua.LATEST.tar.bz2"
-  sha256 "d432357ff459559ef739c0b863a6c36f84e975b0bd284b42435b12cc6763061b"
+  head "https://github.com/alard/wget-lua.git", :branch => "lua"
+  keg_only "too lazy to figure out make install renames"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -9,6 +9,9 @@ class WgetLua < Formula
   depends_on "gettext"
   depends_on "pkg-config" => :build
   depends_on "openssl" => :recommended
+  depends_on "lua"
+
+  patch :DATA
 
   def install
     # Fixes undefined symbols _iconv, _iconv_close, _iconv_open
@@ -26,11 +29,33 @@ class WgetLua < Formula
 
     system "./bootstrap" if build.head?
     system "./configure", *args
-    system "make"
-    cp "src/wget", "#{bin}/wget-lua"
+    system "make", "install"
   end
 
   test do
     system "#{bin}/wget-lua -V | grep -qi 'wget\+lua'"
   end
 end
+
+__END__
+diff --git a/src/luahooks.c b/src/luahooks.c
+index 2ec5239..4ab4f36 100644
+--- a/src/luahooks.c
++++ b/src/luahooks.c
+@@ -145,7 +145,7 @@ luahooks_init ()
+   if (opt.lua_filename == NULL)
+     return;
+
+-  lua = lua_open ();
++  lua = luaL_newstate ();
+   luaL_openlibs (lua);
+
+   /* Initialize constants. */
+@@ -489,7 +489,7 @@ luahooks_lookup_host (const char *host)
+         return NULL;
+
+       /* Copy to the buffer. */
+-      size_t ret_l = lua_strlen(lua, -1);
++      size_t ret_l = lua_rawlen(lua, -1);
+       ret_l = (ret_l <= MAX_HOST_LENGTH) ? ret_l : MAX_HOST_LENGTH;
+       strncpy (lookup_host_result, ret, ret_l);
